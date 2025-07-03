5cf17adb79d60a025361d201291602b10046e855
amank010 <amanksingh010@gmail.com>
2025-07-04 04:13:41 +0530

    welcome screen

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="text-center justify-center flex flex-col h-full items-center space-y-4">
+      <Image
+        src="/logo.png"
+        alt="notion"
+        className="object-contain"
+        width={200}
+        height={200}
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to Notion, {user?.firstName}
+      </h2>
+      <Button>
+        <PlusCircle />
+        Create a note
+      </Button>
     </div>
   );
 };
