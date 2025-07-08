d822467ebdc421459b41a770a182775a668f6c92
amank010 <amanksingh010@gmail.com>
2025-07-08 15:23:28 +0530

    navbar, change untitled name in real time

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Document</h1>
+    </div>
+  );
+};
+
+export default page;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ComponentRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
@@ -25,6 +25,7 @@ import { PopoverContent } from "@radix-ui/react-popover";
 import TrashBox from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import Navbar from "./navbar";
 
 export const Navigation = () => {
   const search = useSearch();
@@ -38,6 +39,8 @@ export const Navigation = () => {
   const settings = useSettings();
 
   const create = useMutation(api.documents.create);
+
+  const params = useParams();
 
   useEffect(() => {
     if (isMobile) {
@@ -178,15 +181,19 @@ export const Navigation = () => {
           isMobile && "left-0 w-full",
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onRestWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
